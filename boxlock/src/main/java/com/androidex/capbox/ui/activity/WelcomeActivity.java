c3b7797fc5f3824d3f8e8eddabc4a72d87c6b99a
liyp <liyp@androidex.cn>
2018-01-03 21:03:24 +0800

    完善启动页

--- boxlock/src/main/java/com/androidex/capbox/ui/activity/WelcomeActivity.java
+++ boxlock/src/main/java/com/androidex/capbox/ui/activity/WelcomeActivity.java
@@ -1,24 +1,29 @@
 package com.androidex.capbox.ui.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 import com.androidex.capbox.R;
 import com.androidex.capbox.base.UserBaseActivity;
 
 public class WelcomeActivity extends UserBaseActivity {
+    private final int SPLASH_DISPLAY_LENGHT = 3000;
+    private Handler handler;
 
     @Override
     public void initData(Bundle savedInstanceState) {
         //在manifest里面配置：直接进入全屏
         //<activity android:theme="@android:style/Theme.NoTitleBar.Fullscreen" /> 只在当前Activity内显示全屏
         // <application android:theme="@android:style/Theme.NoTitleBar.Fullscreen" /> 为整个应用配置全屏显示
-        try {
-            Thread.sleep(2000);
-            FingerprintMainActivity.lauch(context);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        handler = new Handler();
+        // 延迟SPLASH_DISPLAY_LENGHT时间然后跳转到MainActivity
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FingerprintMainActivity.lauch(context);
+            }
+        }, SPLASH_DISPLAY_LENGHT);
     }
 
     @Override
