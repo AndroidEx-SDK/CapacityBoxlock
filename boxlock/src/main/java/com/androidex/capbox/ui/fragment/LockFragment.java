220d9aa537909168a075639ebeb316e9b0c6d70a
liyp <liyp@androidex.cn>
2018-08-17 18:18:10 +0800

    no message

--- boxlock/src/main/java/com/androidex/capbox/ui/fragment/LockFragment.java
+++ boxlock/src/main/java/com/androidex/capbox/ui/fragment/LockFragment.java
@@ -35,7 +35,6 @@ import com.androidex.capbox.service.MyBleService;
 import com.androidex.capbox.ui.activity.BoxDetailActivity;
 import com.androidex.capbox.ui.activity.ConnectDeviceListActivity;
 import com.androidex.capbox.ui.activity.DebugBLEActivity;
-import com.androidex.capbox.ui.view.GrayScaleImageView;
 import com.androidex.capbox.ui.view.TitlePopup;
 import com.androidex.capbox.utils.BleUpdateUtil;
 import com.androidex.capbox.utils.CalendarUtil;
